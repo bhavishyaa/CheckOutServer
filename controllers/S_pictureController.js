5746b8d37c165716f8d5b72fbcd1044e05e57884
Nishanth Kabra <nishantkabra14@gmail.com>
2015-08-26 15:50:45 +0530

    Manual Update

--- controllers/S_pictureController.js
+++ controllers/S_pictureController.js
@@ -1,22 +1,37 @@
-﻿(function (S_pictureController) {
+(function (S_pictureController) {
 
     var services = require("../services");
     
     S_pictureController.init = function (app) {
         
         app.post("/api/user/userId/imagenew", function (req, res) {
+            var userId = req.headers.userid;
+            //ToDo: check for null
+            if (!userId) {
+                return res.status(401).send({ message : "UserId is not passed in headers" });
+            }
             var picturereq = req;
-            services.postUserPic(picturereq, function (err, results) {
-                res.send(results);
+            res.headers = req.headers;
+
+            var promise = services.postUserPic(userId, picturereq);
+            promise.then(function (result) {
+                res.headers = { 'Content-Type': undefined };
+                res.send(result);
+            }, function (error) {
+                res.send("Not able to post user photo" + picturereq + "error: " + error);
             });
+
         });
         
         app.get("/api/user/userId/getimage", function (req, res) {
-            function servicecallback(results) {
-                res.writeHead(200, { 'Content-Type': 'image/png' });
-                res.end(results, 'binary');
-            }
-            services.getUserPic(servicecallback);
+
+            var promise = services.getUserPic();
+            promise.then(function (result) {
+                res.headers = { 'Content-Type': undefined };
+                res.send(result);
+            }, function (error) {
+                res.send("Not able to get user photo" + "error: " + error);
+            });
         });
 
     };
